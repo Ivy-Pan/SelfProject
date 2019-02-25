ef6bd77eb15aa99a8c267ea99a4f046e1d1cff99
ivyPan <643936369@qq.com>
2019-02-25 20:02:39 +0800

    add or update

--- wx/myClassLibrary/test/test.js
+++ wx/myClassLibrary/test/test.js
@@ -1,59 +1,21 @@
 // test/test.js
 
 import Storage from '../lib/storage.js';
+import Event from '../lib/Event.js';
 
-Page({
-  
-  data: {
+let event = new Event();
 
-  },
-  
-  onLoad: function (options) {
-	  /**
-	   * Storage ：本地 storage 封装测试
-	   * -----------------------------------------------------------
-	   */
-	  // let test = new Storage('test');
-	  // test.add({ id: 5 }).save();
-	  // let data = test.where('id', '>', 0).findAll()
-	  // test.where('id', 5).del().save();
-	  // console.log(data)
-	  // test.where('id', '>', 5).update({id: 6, name:'ivy'}).save()
-	  // test.add({ id: 7 })
-	  // test.where('id', '>=', 5).update({name: 'ivy', age: 18}).save()
-	  // test.where('id', 0).del().save()
-	  // let data = test.where('id', '>', 1).findAll().sort(function(a, b) {
-	  //   return a.id - b.id
-	  // })
-	  // let data = test.where('id', '=', 1).update([1,2])
-	  // console.log(data)
-  },
-  
-  onReady: function () {
-
-  },
-  
-  onShow: function () {
-
-  },
-  
-  onHide: function () {
-
-  },
-
-  onUnload: function () {
-
-  },
-  
-  onPullDownRefresh: function () {
-
-  },
-  
-  onReachBottom: function () {
-
-  },
-  
-  onShareAppMessage: function () {
-
-  }
+event.addEvent('onLoad', () => {
+  console.log(1)
 })
+event.addEvent('onLoad', () => {
+  console.log(2)
+})
+
+event.data = {
+  test: 'test'
+}
+
+
+
+Page(event)
